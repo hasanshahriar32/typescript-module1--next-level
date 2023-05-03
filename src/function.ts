6ae658bf426e84ae6cfd243a86eea8a0c6ca6709
Shahriar Hasan <hasanshahriar32@gmail.com>
2023-05-03 21:58:52 +0600

    function added for ts

---
+++ src/function.ts
@@ -0,0 +1,7 @@
+//normal function
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(1, 434));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const course: string = "next level website building course";
 console.log(course);
 
-import "./basic_types";
-import "./array&tuples";
-import "./object";
+// import "./basic_types";
+// import "./array&tuples";
+// import "./object";
+import "./function";
