e351c71ded2702e0405f6a24e0ab1743cdd97dc2
Shahriar Hasan <hasanshahriar32@gmail.com>
2023-05-05 02:58:26 +0600

    new function added

--- src/function.ts
+++ src/function.ts
@@ -5,3 +5,16 @@ function add(num1, num2) {
 }
 
 console.log(add(1, 434));
+
+// here the params types are not declared explicitly, so these are called any type
+// add(" 2"+2)
+
+// better way to do this using ts
+
+function add1(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(add1(1, 434));
+
+const addarrow = (num1: number, num2: number): number => num1 + num2;
